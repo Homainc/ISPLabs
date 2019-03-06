11533bea9f8b8964e14e81447878f198c98231cd
Филипп Хамицевич <spritefok@gmail.com>
2019-03-06 14:00:39 +0300

    plugged "get_partition_eager.sql"

--- ISPLabs/Models/Category.cs
+++ ISPLabs/Models/Category.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentNHibernate.Mapping;
+﻿using System.Collections.Generic;
 
 namespace ISPLabs.Models
 {
@@ -11,11 +7,25 @@ namespace ISPLabs.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int PartitionId { get; set; }
         public Partition Partition { get; set; }
-        public ISet<Topic> Topics { get; set; }
+        public ICollection<Topic> Topics { get; set; }
+        public int TopicCount { get; set; }
+
         public Category()
         {
-            Topics = new HashSet<Topic>();
+            Topics = new List<Topic>();
+        }
+
+        public Category(int id, string name, string description, int topicCount,Partition partition)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Partition = new Partition();
+            this.PartitionId = partition.Id;
+            this.TopicCount = topicCount;
+            this.Topics = new List<Topic>();
         }
     }
 }
--- ISPLabs/Models/Topic.cs
+++ ISPLabs/Models/Topic.cs
@@ -1,36 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentNHibernate.Mapping;
 
 namespace ISPLabs.Models
 {
     public class Topic
     {
-        public virtual int Id { get; set; }
-        public virtual string Name { get; set; }
-        public virtual bool IsClosed { get; set; }
-        public virtual DateTime Date { get; set; }
-        public virtual Category Category { get; set; }
-        public virtual ISet<ForumMessage> Messages { get; set; }
-        public virtual User User { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsClosed { get; set; }
+        public DateTime Date { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        public ICollection<ForumMessage> Messages { get; set; }
+        public User User { get; set; }
+
         public Topic()
         {
-            Messages = new HashSet<ForumMessage>();
-        }
-    }
-    public class TopicMap : ClassMap<Topic>
-    {
-        public TopicMap()
-        {
-            Id(x => x.Id);
-            Map(x => x.Name);
-            Map(x => x.Date);
-            Map(x => x.IsClosed);
-            References(x => x.Category).Cascade.SaveUpdate();
-            HasMany(x => x.Messages).Cascade.All().Inverse();
-            References(x => x.User).Cascade.SaveUpdate();
+            Messages = new List<ForumMessage>();
+            Category = new Category();
         }
     }
 }
