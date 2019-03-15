c450ab808a28d79dd58917853be1c904f3f7efe6
Филипп Хамицевич <spritefok@gmail.com>
2019-03-15 20:06:17 +0300

    Remove Dapper and NHibernate libraries

--- ISPLabs/Hubs/MessageHub.cs
+++ ISPLabs/Hubs/MessageHub.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
-using NHibernate;
 using ISPLabs.Services;
 using ISPLabs.Models;
 using ISPLabs.Models.API;
